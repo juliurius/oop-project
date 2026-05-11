adb39b226cadaade4cc6f550da967642901c3d75
Maciej-A <148791327+Maciej-A@users.noreply.github.com>
2026-05-11 19:38:53 +0200

    Delete src/main/java/tcsball/TestLauncher.java

--- src/main/java/tcsball/TestLauncher.java
+++
@@ -1,9 +0,0 @@
-package tcsball;
-
-public class TestLauncher {
-    public static void main(String[] args) {
-        // Ta klasa ukrywa przed Javą fakt, że startujemy aplikację JavaFX,
-        // dzięki czemu omija błąd z brakującymi komponentami.
-        RendererTestApp.main(args);
-    }
-}
